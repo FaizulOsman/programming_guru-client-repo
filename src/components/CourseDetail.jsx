93567bdb8512dc6bab8870caa0d37300f61978c1
Faizul Osman <faizulosmanrabby@gmail.com>
2022-10-27 19:13:02 +0600

    final commit

--- src/components/CourseDetail.jsx
+++ src/components/CourseDetail.jsx
@@ -15,7 +15,7 @@ const CourseDetail = () => {
         <div className="w-11/12 mx-auto my-20">
           <div className="flex-none md:flex">
             <div className="md:w-5/12 p-10 my-auto">
-              <img src={img} className="w-1/2 md:w-3/4 mx-auto" alt="img" />
+              <img src={img} className="w-3/5 md:w-3/4 mx-auto" alt="img" />
             </div>
             <div className="md:w-7/12 flex flex-col justify-center items-center md:border-l-2 md:pl-10">
               <div className="">
